a0761c7bfb7b85aac9d0b98576277e58dee0327f
VikasShah8218 <vikasshah8218@gmail.com>
2023-02-04 17:40:15 +0530

    stocks bar done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import './Css/App.css';
 import './Css/Footer.css';
 import './Css/Lumsum.css';
 import './Css/Common.css';
+import './Css/Stock.css';
 import Navbar from './component/Navbar';
 import Footer from './component/Footer';
-import Lumsum from './component/Lumsum.js';
+import Lumsum from './component/Lumsum';
+import Stock from './component/Stock';
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Stock />
     <Lumsum />
     <Footer/>
     </>
